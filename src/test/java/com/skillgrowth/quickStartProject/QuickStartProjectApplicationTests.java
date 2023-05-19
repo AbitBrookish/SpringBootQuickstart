46ac2439c42269a67acb121100189eb2e056ef2d
AbitBrookish <brooksjohnson25@gmail.com>
2023-05-19 09:49:58 -0400

    revision

---
+++ src/test/java/com/skillgrowth/quickStartProject/QuickStartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillgrowth.quickStartProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
